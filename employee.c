e0201d146a437fa295782f83452818f63035a0f9
Gaurav <gaurav.kulkarni1999@gmail.com>
2025-07-16 11:32:08 +0530

    First commit

---
+++ employee.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    char name [100];
+    float id , BS , DA, HRA,Tax,TS;
+    printf("Enter the name of the employee :");
+    scanf("%s",&name);
+    printf("Enter employee id:");
+    scanf("%d",&id);
+    printf("Enter basic salary of the employee:");
+    scanf("%d",& BS);  
+}
