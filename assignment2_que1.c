2619bb0e83fd030c39c428994a6911e8bb240a68
Gaurav <gaurav.kulkarni1999@gmail.com>
2025-07-16 13:26:26 +0530

    First commit

---
+++ assignment2_que1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    if(a>0)
+    printf("The number is positive");
+    return 0;
+}
--- employee.c
+++ employee.c
@@ -6,7 +6,27 @@ int main ()
     printf("Enter the name of the employee :");
     scanf("%s",&name);
     printf("Enter employee id:");
-    scanf("%d",&id);
+    scanf("%f",&id);
+    printf("Enter DA(%%) of the employee:");
+    scanf("%f",&DA);
+    printf("Enter HRA(%%) of the employee:");
+    scanf("%f",&HRA);
     printf("Enter basic salary of the employee:");
-    scanf("%d",& BS);  
+    scanf("%f",&BS);
+    if(BS>=100000)
+     Tax=20;
+    else if (BS>=80000)
+     Tax=15;
+    else if (BS>=50000)
+     Tax=10;
+    else if (BS >=30000)
+     Tax=5;
+    else
+     Tax =0;
+    printf("Enter Tax:%.2f%%\n",Tax);
+    
+    TS = BS+((BS*(DA+HRA-Tax))/100);
+    printf("Total salary of the employee is %f",TS);
+
+    return 0;
 }
