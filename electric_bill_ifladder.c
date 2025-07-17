70cea377c0b397c97b8eaf38035e652d731c10a3
Gaurav <gaurav.kulkarni1999@gmail.com>
2025-07-17 11:34:32 +0530

    First commit

--- electric_bill_ifladder.c
+++ electric_bill_ifladder.c
@@ -2,10 +2,10 @@
 int main ()
 {
     int unit;
-    float bill = 0;
+    float bill ;
     printf("Enter total units:");
     scanf("%d",&unit);
-    if (unit <100)
+    if (unit <100) 
     bill = unit * 8;
     else if(unit>100 && unit<=200)
     bill = (100*8)+((unit-100) * 10);
@@ -17,6 +17,6 @@ int main ()
     bill = (100*8)+(100*10)+(100*12) +((unit -400) * 17);
     else 
     bill = (100*8)+(100*10)+(100*12) +((unit -500) *20);
-    printf("ETatal bill is %.2f\n",bill);
+    printf("Tatal bill is %.2f\n",bill);
     return 0;
 }
